226cfe8177806023f464128177df739b532ae9fb
Shoichi Kagawa <e411z7t40w@gmail.com>
2022-11-20 07:16:19 +0000

    Add arrow environments

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,5 +4,5 @@ set -eux
 
 # clone mysql-server source to build myquiver
 if [[ ! -d /workspaces/mysql-server ]]; then
-  git clone -b mysql-8.0.27 https://github.com/mysql/mysql-server /workspaces/mysql-server
+  git clone --depth=1 -b mysql-8.0.27 https://github.com/mysql/mysql-server /workspaces/mysql-server
 fi
